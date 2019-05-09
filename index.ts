1e1d1c5a12becb789e21fee4631efc504c4a821c
nillabobillababolla <samet.alabey@hotmail.com>
2019-05-09 10:50:30 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+// import operators.
+import { of } from 'rxjs';
+import { map,filter,debounceTime,take } from 'rxjs/operators';
+/*
+ *  'of' allows you to deliver values in a sequence
+ *  In this case, it will emit 1,2,3,4,5 in order.
+ */
+const dataSource = of(1, 2, 3, 4, 5);
+
+// subscribe to our source observable
+const subscription = dataSource
+  .pipe(
+    // add 1 to each emitted value
+    map(value => value + 1),
+    //filter values between 3-5
+    filter(value=>value>2 && value<=5),
+    //take first 2 values.
+    take(2),
+    //pause stream for 200ms.
+    debounceTime(200)
+  )
+  // log: 2, 3, 4, 5, 6
+  .subscribe(value => console.log(value));
